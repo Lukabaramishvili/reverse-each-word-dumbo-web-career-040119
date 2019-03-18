bff8435f45cf3550353d3c234c795accef16b45d
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-18 23:57:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str)
-  reversed = str.split()
+  reversed = str.split(" ")
   reversed.collect do |word|
     word.reverse 
   end
-  return reversed.join
+  return reversed.join(" ")
 end 
